aa1eb97d1b533cdb45e2e28946c3c9f3e95677ad
Rachel Bruce <rachelbruce922@gmail.com>
2022-06-04 13:44:05 -0500

    created  function findByArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,7 +3,6 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 //addToCollection
-
 function addToCollection (title , artist , yearPublished) {
     let newAlbum = {
         title ,
@@ -13,7 +12,10 @@ function addToCollection (title , artist , yearPublished) {
     collection.push(newAlbum);
     return newAlbum
 }
+//end addToCollection
 
+
+//adding albums to collection
 console.log('adding new album:' , addToCollection('Back Home Again' , 'John Denver' , 1974));
 
 console.log('adding new album:' , addToCollection('Long Time Coming' , 'Sierrra Ferrell' , 2021));
@@ -26,10 +28,9 @@ console.log('adding new album:' , addToCollection('Rocky Mountain High' , 'John 
 
 console.log('adding new album:' , addToCollection('maybe i will see you at the end of the world' , 'Sydney Sprague' , 2021));
 
-
 console.log('current collection:' , collection);
+//end adding albums to collection
 
-//end addToCollection
 
 //showCollection
 function showCollection (collectionToBeShown) {
@@ -38,6 +39,22 @@ function showCollection (collectionToBeShown) {
         console.log(collectionToBeShown[i].title + ' by ' +collectionToBeShown[i].artist);
     }
 } 
-
+//end showCollection 
 
 console.log(showCollection(collection));
+
+
+//findByArtist
+function findByArtist(art) {
+    let results = [];
+    for (let i=0; i<collection.length; i++) {
+        if (collection[i].artist === art) {
+            results.push(collection[i]);
+        }
+    }
+    return results
+}
+
+console.log(findByArtist('John Denver'));
+console.log(findByArtist('Popcorn Shrimp'));
+console.log(findByArtist('Rainbow Kitten Surprise'));
