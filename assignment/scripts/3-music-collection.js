3c2670ed692565e1c2b1041ca714270cd30961b6
Rachel Bruce <rachelbruce922@gmail.com>
2022-06-04 12:29:37 -0500

    created function addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,16 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection (title , artist , yearPublished) {
+    let newAlbum = {
+        title ,
+        artist ,
+        yearPublished 
+    }
+    collection.push(newAlbum);
+    return newAlbum
+}
+
+console.log('adding new album:' , addToCollection('Back Home Again' , 'John Denver' , 1974));
+console.log(collection);
