8adc310c5b6fbeab80045471818dd4c4ed330028
Rachel Bruce <rachelbruce922@gmail.com>
2022-06-04 12:50:51 -0500

    added to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -13,4 +13,18 @@ function addToCollection (title , artist , yearPublished) {
 }
 
 console.log('adding new album:' , addToCollection('Back Home Again' , 'John Denver' , 1974));
-console.log(collection);
+
+console.log('adding new album:' , addToCollection('Long Time Coming' , 'Sierrra Ferrell' , 2021));
+
+console.log('adding new album:' , addToCollection('Cleopatra' , 'The Lumineers' , 2016));
+
+console.log('adding new album:' , addToCollection('Seven + Mary' , 'Rainbow Kitten Surprise' , 2013));
+
+console.log('adding new album:' , addToCollection('Rocky Mountain High' , 'John Denver' , 1972));
+
+console.log('adding new album:' , addToCollection('maybe i will see you at the end of the world' , 'Sydney Sprague' , 2021));
+
+
+console.log('current collection:' , collection);
+
+
